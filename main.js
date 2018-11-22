cb0b90f9091db3efeee19c0e2fb2b74fe63e8f00
lygustas <42089835+lygustas@users.noreply.github.com>
2018-11-22 22:36:25 +0800

    addRichtext
    addRichtext

---
+++ main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App'
+// #ifdef H5
+import vcolorpicker from 'vcolorpicker'
+Vue.use(vcolorpicker)
+// #endif
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+const app = new Vue({
+    ...App
+})
+app.$mount()
