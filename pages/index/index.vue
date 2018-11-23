f5181e30f0b90c6b12b806a34a88faa431f72057
lygustas <42089835+lygustas@users.noreply.github.com>
2018-11-23 19:33:40 +0800

    new
    new

---
+++ pages/index/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		<uni-richtext :richList.sync="richList" :uploadUrl="uploadUrl"></uni-richtext>
+	</view>
+</template>
+
+<script>
+	import uniRichtext from '../../components/uni-richtext.vue'
+	export default {
+		components:{
+			uniRichtext
+		},
+		data() {
+			return {
+				richList: [],
+				uploadUrl:"http://120.78.87.84:8080/conduit/file/uploadFile"
+			}
+		},
+		watch:{
+			richList:function (newValue,oldValue) {
+				console.log(newValue)
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
